ed24f52dfe4d36220a5834c813b9a53007b3c3ad
Przemek Hocke <phocke@gmail.com>
2012-01-22 20:49:13 +0000

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhockeTracking::Application.config.secret_token = 'bb367dacc10212fa92efa0c7f55293d23972afb9dd2246468686408c914a1bc0ca9ed6a147365fb1a6c3b44c0984582b33d56a2be9bb53f6ba06122b02d48d03'
