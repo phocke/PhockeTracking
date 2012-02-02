e7f90f70911a26d5a6563329ec94a3206b3dac02
Przemek Hocke <phocke@gmail.com>
2012-02-02 01:29:38 +0000

    use real data rather than placeholders

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,33 +2,15 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-window.onload = function () {
-    var r = Raphael("holder")
+$('document').ready(function(){
+  var r = Raphael("holder")
 
-        results = [13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
-                   12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
-                   11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
-                   10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-                   9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,
-                   8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,
-                   7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,
-                   6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,
-                   5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,              
-                   4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,
-                   3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,
-                   2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
-                   1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1]
+  r.dotchart(0, 0, 1000, 800, which_day, which_habit, results, {symbol: "0", max: 10, heat: true, axis: "0 0 1 1", axisxstep: dates.length-1, axisystep: habits.length-1, axisxlabels: dates, axisxtype: " ", axisytype: " ", axisylabels: habits}).hover(function () {
+      this.marker = this.marker || r.tag(this.x, this.y, this.value, 0, this.r + 2).insertBefore(this);
+      this.marker.show();
+  }, function () {
+      this.marker && this.marker.hide();
+  });
 
-        habits = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "sup", "du", "hu", "sa", "sat", "dsa"]
-        // dates = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
-        //           "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", 
-        //           "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"];
-
-    r.dotchart(0, 0, 1000, 300, which_day, which_habit, results, {symbol: "o", max: 10, heat: true, axis: "0 0 1 1", axisxstep: dates.length-1, axisystep: habits.length-1, axisxlabels: dates, axisxtype: " ", axisytype: " ", axisylabels: habits}).hover(function () {
-        this.marker = this.marker || r.tag(this.x, this.y, this.value, 0, this.r + 2).insertBefore(this);
-        this.marker.show();
-    }, function () {
-        this.marker && this.marker.hide();
-    });
-};
-     
+  $('svg').attr('width', 1000).attr('height', 800);
+})     
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,62 +1,71 @@
 require "csv"
 class Result
 
-  attr_accessor :headers, :results
+  attr_accessor :results, :selected_habits
 	
   CSV_PATH = File.expand_path(Rails.root.to_s + "/db/tracking.csv")
 
   def initialize
+    @results = {}
+    @selected_habits = YAML::load( File.open Rails.root.to_s + "/config/tracked-habits.yml" )[:now]
     load_headers
-    create_attribute_methods
     load_results
   end
 
-  def selfclass
-    (class << self; self; end)
-  end
-
-  def create_attribute_methods
-    @headers.each do |header|
-      selfclass.send(:define_method, header) do |date|
-        index = 2
-
-        results[date][index]
-      end
-    end
-  end
-
   def load_results
-    @results = {}
-
-    rows = CSV.read(CSV_PATH)
-    rows.shift
-
-    rows.each do |result|
+    CSV.foreach CSV_PATH,  headers: true do |results|
       # "1/22/2012 1:40:48"
       # it is parsed incorrectly - it swaps :mday with :mon
-      timestamp = Date._parse result[0]
-      date = Date.new(timestamp[:year], timestamp[:mday], timestamp[:mon]).to_s
-      @results[date] = result[1, result.size] #get rid of timestamp
+      # timestamp = Date._parse result[0]
+      # date = Date.new(timestamp[:year], timestamp[:mday], timestamp[:mon]).to_s
+      # @results[date] = result[1, result.size] #get rid of timestamp
+      results = results.to_a
+      results = results.map do |pair| 
+        pair[0] = sanitize_attribute_name pair[0]
+        pair[1] = digitize pair[1]
+        pair
+      end
+      results = Hash[results]
+      results.each{|key, value| @results[key] << value}
     end
   end
 
   def load_headers
-    @headers = CSV.read(CSV_PATH).to_a.shift.collect do |name|
-      sanitize_attribute_name name
-    end 
+    CSV.read(CSV_PATH).to_a.shift.collect do |name|
+      @results[sanitize_attribute_name name] = []
+    end
   end
 
   def sanitize_attribute_name(attribute)
-    attribute.strip.gsub(/[^0-9A-Za-z.\-]+/, '_').gsub(/_$/, '').downcase
+    attribute.strip.gsub(/[^0-9A-Za-z.\-]+/, '_').gsub(/_$/, '').downcase.to_sym
   end
 
   def which_habit
-    arr = []; (1..13).each{|num| 30.times{ arr << num}}
+    arr = []; (1..13).each{|num| 10.times{ arr << num}}
     arr.reverse
   end
 
+  def habits
+    @selected_habits.map do |habit_name| 
+      habit_name.to_s[0,40]
+    end
+  end
+
+  def digitize(string)
+    return 1 unless string and string.strip =~ /yes/
+    return 3
+  end
+
   def which_day
-    a = []; 13.times{a << (0..29).to_a}
+    a = []; 13.times{a << (1..10).to_a}
     a.flatten
+  end
+
+  def output
+    a = []
+    @selected_habits.each do |habit|
+      a << @results[habit]
+    end
+    return a.flatten
   end
 end
